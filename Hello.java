15fff25296c1c302667058f02462ad268539a54e
mldn <mldnqa@163.com>
2018-08-27 18:14:04 +0800

    commit Hello.java File

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello{
+	public static void main(String args[]){
+		System.out.println("这是一个小程序A") ;
+		System.out.println("这是一个小程序B") ;
+		System.out.println("这是一个小程序C") ;
+		System.out.println("这是一个小程序D") ;
+		System.out.println("这是一个小程序E") ;
+		System.out.println("这是一个小程序F") ;
+	}
+}
